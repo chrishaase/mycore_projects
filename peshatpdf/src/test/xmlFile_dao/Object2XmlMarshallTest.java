3a9e31c8d99ac4615f5bf616af7922e704900a3d
sigsigsputnik <haase.mail@web.de>
2019-01-23 14:56:27 +0100

    tests

--- peshatpdf/src/test/xmlFile_dao/Object2XmlMarshallTest.java
+++ peshatpdf/src/test/xmlFile_dao/Object2XmlMarshallTest.java
@@ -136,42 +136,5 @@ public class Object2XmlMarshallTest {
 
 
 
-    /*
-    Beispiel Programm
-    {
-        employees.setEmployees(new ArrayList<Employee>());
-        //Create two employees
-        Employee emp1 = new Employee();
-        emp1.setId(1);
-        emp1.setFirstName("Lokesh");
-        emp1.setLastName("Gupta");
-        emp1.setIncome(100.0);
-
-        Employee emp2 = new Employee();
-        emp2.setId(2);
-        emp2.setFirstName("John");
-        emp2.setLastName("Mclane");
-        emp2.setIncome(200.0);
-
-        //Add the employees in list
-        employees.getEmployees().add(emp1);
-        employees.getEmployees().add(emp2);
-    }
-
-    private static void marshalingExample() throws JAXBException
-    {
-        JAXBContext jaxbContext = JAXBContext.newInstance(Employees.class);
-        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-
-        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-        //Marshal the employees list in console
-        jaxbMarshaller.marshal(employees, System.out);
-
-        //Marshal the employees list in file
-        jaxbMarshaller.marshal(employees, new File("c:/temp/employees.xml"));
-    }
-
-    */
 
 }
